21ccf3e79993429c34829085b006a9ab5341ce05
Jaman-dedy <jeandedieuam@gmail.com>
2024-07-30 21:08:40 +0200

    cache all the ignored files

--- app/crud/__init__.py
+++
@@ -1,4 +0,0 @@
-from .user import get_user_interactions
-from .hashtag import get_hashtag_score
-from .tweet import get_keywords_score
-from .crud import get_user_recommendations
--- app/schemas/__init__.py
+++
@@ -1,9 +0,0 @@
-from .schemas import (
-    User, UserCreate, 
-    Tweet, TweetCreate, 
-    Hashtag, HashtagCreate, 
-    UserInteraction, UserInteractionCreate, 
-    HashtagScore, HashtagScoreCreate, 
-    HashtagFrequency, HashtagFrequencyCreate,
-    UserRecommendation
-)
